3e90737659d9901580ea73a134048d2ae05ad780
Viclinh <stevenwonder19@gmail.com>
2023-02-15 22:41:06 -0600

    first commit

---
+++ TravelryMessage/index.js
@@ -0,0 +1,29 @@
+// Dependencies to install:
+// $ npm install node-fetch --save
+
+const fetch = require('node-fetch');
+
+const options = {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer pk_prod_3KTR29GG8E4CNWGZT7D7F2HKMGTF'
+  },
+  body: JSON.stringify({
+    "message": {
+      "to": {
+        "email": "testhong30@gmail.com"
+      },
+      "content": {
+        "body": "Thanks for subscribing to us. We will send you discounted emails about our travel packages.",
+        "title": "Welcome to Travelry"
+      }
+    }
+  })
+};
+
+fetch('https://api.courier.com/send', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
